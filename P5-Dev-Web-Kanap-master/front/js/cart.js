b5eddbb522b5d8b5fe25e92be7c2ab8cfadd890a
CurtCorentin <108574301+CurtCorentin@users.noreply.github.com>
2022-08-10 00:00:51 +0200

    ...

--- P5-Dev-Web-Kanap-master/front/js/cart.js
+++ P5-Dev-Web-Kanap-master/front/js/cart.js
@@ -1,28 +1,22 @@
 let ADDPRODUITS = JSON.parse(localStorage.getItem("produit"));
 console.log(ADDPRODUITS);
 
-let totalPanier = ADDPRODUITS[i].quantite * ADDPRODUITS[i].price ;
-
 const DISPLAYPRODUITS = () => {
-
-  
-for(let i = 0; i < ADDPRODUITS.length; i++) {
-
 cart__items.innerHTML = ADDPRODUITS.map((produit) =>
-`<article class="cart__item" data-id="${ADDPRODUITS._id}" data-color="${ADDPRODUITS[i].couleur}">
+`<article class="cart__item" data-id="${produit._id}" data-color="${produit.couleur}">
 <div class="cart__item__img">
-  <img src="${ADDPRODUITS[0].imageUrl}" alt="${ADDPRODUITS[i].altTxt}">
+  <img src="${produit.imageUrl}" alt="${produit.altTxt}">
 </div>
 <div class="cart__item__content">
   <div class="cart__item__content__description">
-    <h2>${ADDPRODUITS[1].name}</h2>
-    <p>${ADDPRODUITS[i].couleur}</p>
-    <p>${ADDPRODUITS[i].price}$</p>
+    <h2>${produit.name}</h2>
+    <p>${produit.couleur}</p>
+    <p>${produit.price}$</p>
   </div>
   <div class="cart__item__content__settings">
     <div class="cart__item__content__settings__quantity">
       <p>Qté : </p>
-      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${ADDPRODUITS[i].quantite}">
+      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${produit.quantite}">
     </div>
     <div class="cart__item__content__settings__delete">
       <p class="deleteItem">Supprimer</p>
@@ -30,10 +24,8 @@ cart__items.innerHTML = ADDPRODUITS.map((produit) =>
   </div>
 </div>
 </article>`,);
-
+let totalPanier = ADDPRODUITS.quantite * ADDPRODUITS.price ;
 document.getElementById("totalPrice").innerText = `${totalPanier}`;
-document.getElementById("totalQuantity").innerText = `${ADDPRODUITS[i].quantite}`;
-}
+document.getElementById("totalQuantity").innerText = `${produit.quantite}`;
 };
-
 DISPLAYPRODUITS();
