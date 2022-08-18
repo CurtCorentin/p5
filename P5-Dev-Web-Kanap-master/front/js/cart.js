a55123a3d01d893dedcd20fc939a1a3aa649c1c4
CurtCorentin <108574301+CurtCorentin@users.noreply.github.com>
2022-08-18 03:59:53 +0200

    cccc

--- P5-Dev-Web-Kanap-master/front/js/cart.js
+++ P5-Dev-Web-Kanap-master/front/js/cart.js
@@ -1,70 +1,88 @@
-let ADDPRODUITS = JSON.parse(localStorage.getItem("produit"));
-console.log(ADDPRODUITS);
+let ADDPRODUITS = [];
 
-const DISPLAYPRODUITS = () => {
-cart__items.innerHTML = ADDPRODUITS.map((produit) =>
-`<article class="cart__item" data-id="${produit._id}" data-color="${produit.couleur}">
-<div class="cart__item__img">
-  <img src="${produit.imageUrl}" alt="${produit.altTxt}">
-</div>
-<div class="cart__item__content">
-  <div class="cart__item__content__description">
-    <h2>${produit.name}</h2>
-    <p>${produit.couleur}</p>
-    <p>${produit.price}$</p>
-  </div>
-  <div class="cart__item__content__settings">
-    <div class="cart__item__content__settings__quantity">
-      <p>Qté : </p>
-      <input type="number" class="itemQuantity" id="itemQuantity" name="itemQuantity" min="1" max="100" value="${produit.quantite}">
-    </div>
-    <div class="cart__item__content__settings__delete">
-      <p class="deleteItem" id="${produit._id}" >Supprimer</p>
-    </div>
-  </div>
-</div>
-</article>`,).join("");
+const FETCHPRODUITS = async () => { 
+ await fetch(`http://localhost:3000/api/products`)
+  .then((res) => res.json())
+  .then((promise) => {
+      produitData = promise;});
 };
-DISPLAYPRODUITS();
+  FETCHPRODUITS();
 
+  Recuperation =async () => {
+    await FETCHPRODUITS();
+    console.log(produitData);
+      for (let i = 0; i < produitData.length; i++ ) {for (let j = 0; j < produitData[i].colors.length; j++) {
 
-///////////////////////////////////////////////////////////////////////////////////////////////
+        let addproduit = JSON.parse(localStorage.getItem(produitData[i]._id+produitData[i].colors[j]));
+        if (addproduit == null){
+}else{
+  ADDPRODUITS.push(addproduit);
+}}
+}
+
+console.log(ADDPRODUITS,"1111");
+
+cart__items.innerHTML = ADDPRODUITS.map((produit) => 
+ `<article class="cart__item" data-id="${produit._id}" data-color="${produit.couleur}">
+ <div class="cart__item__img">
+   <img src="${produit.imageUrl}" alt="${produit.altTxt}">
+ </div>
+ <div class="cart__item__content">
+   <div class="cart__item__content__description">
+     <h2>${produit.name}</h2>
+     <p>${produit.couleur}</p>
+     <p>${produit.price}$</p>
+   </div>
+   <div class="cart__item__content__settings">
+     <div class="cart__item__content__settings__quantity">
+       <p>Qté : </p>
+       <input type="number" class="itemQuantity" id="itemQuantity" name="itemQuantity" min="1" max="100" value="${produit.quantite}">
+     </div>
+     <div class="cart__item__content__settings__delete">
+       <p class="deleteItem" id="${produit._id}" >Supprimer</p>
+     </div>
+   </div>
+ </div>
+ </article>`,).join("");
+ ///////////////////////////////////////////////////////////////////////////////////////////////
 ///////////Manipulation panier
-let Boutons = [];
- let boutons = document.getElementById("itemQuantity");
- Boutons.push(boutons);
-// console.log(Boutons,"rxgxg");
-Boutons.map((bouton)=>{
- // console.log(bouton.dataset,"UYUTYKUYVKUYVK"); 
-});
- 
+      let boutons = document.getElementsByClassName("itemQuantity");
+       for (let b = 0; b < boutons.length; b++) {
+     boutons[b].addEventListener("input",() => {
+      console.log(boutons[b].value);
+     })};
 
 /////////////////////////////////////////::::::
 /////Supprimer produit
+
   let poubelles = document.getElementsByClassName("deleteItem");
-  let Poubelles = [];
- Poubelles.push(poubelles);
-Poubelles.map((poubelle) =>{
-  console.log("poubelle(s)",poubelle);
-});
- 
+  for (let p = 0; p < poubelles.length; p++) {
+    poubelles[p].addEventListener("click",() => { 
+  console.log(poubelles[p]);})}
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 //Total panier
 let quantitePanier = [];
 let prixPanier = []; 
-ADDPRODUITS.map((produit) =>
-{ 
+ADDPRODUITS.map((produit) => { 
 let calculPanier = (produit.price * produit.quantite);
   prixPanier.push(calculPanier);
   quantitePanier.push(produit.quantite);
-  //console.log(quantitePanier);
-  //console.log(prixPanier);
-//  console.log(calculPanier);
   totalQuantity.textContent = `${eval(quantitePanier.join("+"))}`;
 totalPrice.textContent = `${eval(prixPanier.join("+"))}`;
 });
+};
 
+   Recuperation();
+
+
+
+
+
+ 
+
+
+ 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////Formulaire
@@ -74,16 +92,15 @@ totalPrice.textContent = `${eval(prixPanier.join("+"))}`;
   const VILLE = document.getElementById("city");
   const EMAIL = document.getElementById("email");
   const FORMULAIRE =[];
+
   const COMMANDER = document.getElementById("order");
-console.log(COMMANDER.formAction);
 COMMANDER.formAction = "./confirmation.html";
-console.log(COMMANDER.formAction);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////Controle value
 PRENOM.addEventListener("input",function(e) {
   let valuePrenom = e.target.value ;
-  //console.log(valuePrenom);
+ console.log(valuePrenom);
 });
 NOM.addEventListener("input",function(e) {
   let valueNom = e.target.value;
@@ -109,9 +126,7 @@ EMAIL.addEventListener("input",function(e) {
     if (PRENOM.value,NOM.value,ADRESSE.value,VILLE.value,EMAIL.value != ("")  ) {
   FORMULAIRE.push(PRENOM.value,NOM.value,ADRESSE.value,VILLE.value,EMAIL.value); 
   localStorage.setItem("formulaire",JSON.stringify(FORMULAIRE));
-  console.log("Commande envoyer");
 }else {console.log("Remplir le formulaire")}});
 
-console.log("dsddsd",localStorage.getItem("formulaire"))
 
 
--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -2,33 +2,28 @@
 const PRODUIT = window.location.search.split('?').join("")  ;
 console.log(PRODUIT) ;
 
-let produitData = [];
+
 
 const FETCHPRODUITS = async () => { 
     await fetch(`http://localhost:3000/api/products/${PRODUIT}`)
     .then((res) => res.json())
     .then((promise) => {
-        produitData = promise;});
-        };
-
+        produitData = promise;});};
     FETCHPRODUITS ();
 
-    const DISPLAYPRODUITS = async () => 
-    { 
+    const DISPLAYPRODUITS = async () =>  { 
     await FETCHPRODUITS(); 
  IMAGE.innerHTML = ` <img src="${produitData.imageUrl}" alt="${produitData.altTxt}"/>`;
    title.innerText = `${produitData.name.toUpperCase()} `;
    price.innerText = `${produitData.price} `;
    description.innerText = `${produitData.description} `;
   let select = document.getElementById("colors");
- produitData.colors.forEach((color) => 
- {
+ produitData.colors.forEach((color) => {
     document.createElement("option");
     let tagOption = document.createElement("option");
     tagOption.innerHTML = ` ${color}`;
     tagOption.value=`${color}`;
-    select.appendChild(tagOption);
- })
+    select.appendChild(tagOption); })
 
 };
 
@@ -36,52 +31,37 @@ DISPLAYPRODUITS ();
 
 
 
-const envoieAuPanier = () => 
-{
+const envoieAuPanier = async () => {
+    
  let bouton = document.getElementById("addToCart");
- bouton.addEventListener("click",() => 
- {
-    let select = document.getElementById("colors");
-    let quantite = document.getElementById("quantity");
+ let select = document.getElementById("colors");
+ let quantite = document.getElementById("quantity");
 
-const FUSIONPRODUITCOULEUR = [
-    `${select.value}`,
-    `${quantite.value}` ];
+ bouton.addEventListener("click",() => {
 
     if (quantite.value <= 0 ) 
     {
         return false;
     } 
-    if (localStorage.getItem(PRODUIT+select.value) != null  )
-                { let quantiteActuel =                  parseInt(JSON.parse(localStorage.getItem(PRODUIT+select.value))[1],10);
-       //  `${eval((localStorage.getItem(PRODUIT.value+select.value).join("+"))}`
-                 quantite.value =  quantite.value + quantiteActuel ;
-                   
-                return(
-                   
+    if (localStorage.getItem(PRODUIT+select.value) != null  ) { 
+        let quantiteActuel = JSON.parse(localStorage.getItem(PRODUIT+select.value)).quantite;
 
-                        localStorage.setItem(PRODUIT+select.value,JSON.stringify(FUSIONPRODUITCOULEUR),
-                
-        
-                        ));
-                }
-                 else {return(
-                            console.log("nouveau") ,
-                            localStorage.setItem(PRODUIT+select.value,JSON.stringify(FUSIONPRODUITCOULEUR)),
-                            console.log(localStorage.getItem(PRODUIT+select.value)),
-                            console.log(localStorage.getItem(PRODUIT[0])),
-                            console.log(localStorage.getItem(PRODUIT[1]))
-                            ); }
-})
-};
+
+        let quantiteTableau = [];
+        quantiteTableau.push(quantite.value,quantiteActuel);  
+       let quantiteFinal= `${eval(quantiteTableau.join("+"))}`;
+        let majPanier = {couleur:select.value,quantite:quantiteFinal};
+       
+        localStorage.setItem(PRODUIT+select.value,JSON.stringify(Object.assign(majPanier,produitData)) );
+         console.log(JSON.parse(localStorage.getItem(PRODUIT+select.value)));
+    } else {
+        console.log("nouveau") ;
+
+        let nouveauPanier = {couleur:select.value,quantite:quantite.value};
+        localStorage.setItem(PRODUIT+select.value,JSON.stringify(Object.assign(nouveauPanier,produitData)));
+
+        console.log(JSON.parse(localStorage.getItem(PRODUIT+select.value)));
+    }})
+    };
+    
 envoieAuPanier();
-
-
-
-/*Etape 1, on reçoit l'identifiant du produit choisi (PRODUIT)
-Vérifier s'il est ou pas dans le localStorage (getItem)
-Etape 2
-Si le getItem donne null
-	Ajouter faire un setItem dans le localStorage avec les nouvelles données
-Si le getItem donne autre chose que null
-	Faire un setItem dans le localStorage en modifiant la quantité par ancienne quantité+nouvelle quantité */
